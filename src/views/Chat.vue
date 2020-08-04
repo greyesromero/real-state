096a95ae0bfc9c19c490859502642d516dddd3ee
Gabriela Reyes <greyesromero.ios@gmail.com>
2020-08-03 18:29:26 -0600

    Chat fixed

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,6 +1,104 @@
 <template>
 	<section id="contact" class="grey lighten-3">
-		<div class = "contenedor">
+		 <v-layout wrap >
+      <v-flex xs4 class="side">
+           <v-card
+				
+					class="mx-auto"
+					style="height:100%!important;"
+				>
+				
+					<v-list three-line>
+					<template v-for="(item, index) in items">
+						<v-subheader
+						v-if="item.header"
+						:key="item.header"
+						v-text="item.header"
+						></v-subheader>
+
+						<v-divider
+						v-else-if="item.divider"
+						:key="index"
+						:inset="item.inset"
+						></v-divider>
+
+						<v-list-item
+						v-else
+						:key="item.title"
+						@click=""
+						>
+						<v-list-item-avatar>
+							<v-img :src="item.avatar"></v-img>
+						</v-list-item-avatar>
+
+						<v-list-item-content>
+							<v-list-item-title v-html="item.title"></v-list-item-title>
+							<v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+						</v-list-item-content>
+						</v-list-item>
+					</template>
+					</v-list>
+				</v-card>
+         
+      </v-flex>
+      <v-flex class="contenido  hidden-sm-and-down" >
+         <v-layout column justify-space-between fill-height>
+			  <v-card
+				color="grey lighten-4"
+				flat
+			
+				tile
+			>
+				<v-toolbar dense>
+
+				<v-toolbar-title>Ali Connors</v-toolbar-title>
+
+				<v-spacer></v-spacer>
+
+				
+				<v-btn icon>
+					<v-icon>mdi-dots-vertical</v-icon>
+				</v-btn>
+				</v-toolbar>
+			</v-card>
+            <v-card flat style="overflow-x:hidden!important;overflow-y:scroll!important;height:100%!important;">
+              	<section class="discussion py-12 px-2" style="margin-bottom:30px;">
+	
+					<div class="bubble sender first">Hello</div>
+					<div class="bubble sender last">This is a CSS demo of the Messenger chat bubbles, that merge when stacked together.</div>
+					
+					<div class="bubble recipient first">Oh that's cool!</div>
+					<div class="bubble recipient last">Did you use JavaScript to perform that kind of effect?</div>
+					
+					<div class="bubble sender first">No, that's full CSS3!</div>
+					<div class="bubble sender middle">(Take a look to the 'JS' section of this Pen... it's empty! 😃</div>
+					<div class="bubble sender last">And it's also really lightweight!</div>
+					
+					<div class="bubble recipient">Dope!</div>
+					
+					<div class="bubble sender first">Yeah, but I still didn't succeed to get rid of these stupid .first and .last classes.</div>
+					<div class="bubble sender middle">The only solution I see is using JS, or a &lt;div&gt; to group elements together, but I don't want to ...</div>
+					<div class="bubble sender last">I think it's more transparent and easier to group .bubble elements in the same parent.</div>
+					
+				</section>
+            </v-card>
+            <v-card class="white px-5">
+               <v-text-field
+							flat
+							label="Escribe un mensaje ..."
+							hide-details
+							solo
+							outlined
+							rounded
+							color="secondary"
+							class=""
+							
+							></v-text-field>
+            </v-card>
+         </v-layout>
+      </v-flex>
+   </v-layout>
+		<!--div class = "contenedor">
 			<div class = "side">
 				<v-card
 				
@@ -39,9 +137,9 @@
 					</v-list>
 				</v-card>
 			</div>
-			<div class ="contenido">
-				
-				<section class="discussion py-12 position-relative" style="margin-bottom:30px;">
+			<div class ="contenido position-relative">
+			
+				<section class="discussion py-12" style="margin-bottom:30px;">
 	
 					<div class="bubble sender first">Hello</div>
 					<div class="bubble sender last">This is a CSS demo of the Messenger chat bubbles, that merge when stacked together.</div>
@@ -58,12 +156,25 @@
 					<div class="bubble sender first">Yeah, but I still didn't succeed to get rid of these stupid .first and .last classes.</div>
 					<div class="bubble sender middle">The only solution I see is using JS, or a &lt;div&gt; to group elements together, but I don't want to ...</div>
 					<div class="bubble sender last">I think it's more transparent and easier to group .bubble elements in the same parent.</div>
+					<div class="px-12 position-relative">
+						<v-text-field
+							flat
+							label="Escribe un mensaje ..."
+							hide-details
+							solo
+							outlined
+							rounded
+							color="secondary"
+							class="prueba position-absolute"
+							
+							></v-text-field>
+					</div>
 				</section>
 				
 				
 			
 			</div>
-		</div>
+		</div-->
 	</section>	
 </template>
 
@@ -85,50 +196,50 @@ export default {
         { header: 'Today' },
         {
           avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-          title: 'Brunch this weekend?',
-          subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+          title: 'Ali Connors',
+          subtitle: " &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
         },
         { divider: true, inset: true },
         {
           avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-          subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.",
+          title: 'Alex, Scott, Jennifer',
+          subtitle: " &mdash; Wish I could come, but I'm out of town this weekend.",
         },
         { divider: true, inset: true },
         {
           avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?",
+          title: 'Sandra Adams',
+          subtitle: "&mdash; Do you have Paris recommendations? Have you ever been?",
         },
         { divider: true, inset: true },
         {
           avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
-          title: 'Birthday gift',
-          subtitle: "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?",
+          title: 'Trevor Hansen',
+          subtitle: " &mdash; Have any ideas about what we should get Heidi for her birthday?",
         },
         { divider: true, inset: true },
         {
           avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-          title: 'Recipe to try',
-          subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+          title: 'Britta Holt',
+          subtitle: " &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
 		},
 		{ divider: true, inset: true },
         {
           avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-          title: 'Recipe to try',
-          subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+          title: 'Britta Holt',
+          subtitle: " &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
 		},
 		{ divider: true, inset: true },
         {
           avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-          title: 'Recipe to try',
-          subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+          title: 'Britta Holt',
+          subtitle: "&mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
 		},
 		{ divider: true, inset: true },
         {
           avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-          title: 'Recipe to try',
-          subtitle: "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+          title: 'Britta Holt',
+          subtitle: "&mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
         },
       ],
    
@@ -243,18 +354,19 @@ export default {
 }
 
 .side{
-  flex: 4;
+ 
   height: 90vh;
   color: white;
   background-color: white;
-  overflow: scroll;
+  overflow-y: scroll;
+  overflow-x: hidden;
 }
 
 .contenido{
   flex: 8;
   height: 90vh;
   background-color: white;
-  overflow: scroll;
+  overflow:hidden!important;
 
 }
 
@@ -277,7 +389,7 @@ export default {
 }
 
 .discussion {
-	max-width: 600px;
+	max-width: 900px;
 	margin: 0 auto;
 	
 	display: flex;
@@ -352,6 +464,33 @@ export default {
     border-top-left-radius: 5px;
 }
 
+.prueba{
+	
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    width: auto;
+    height: 50px;
+    z-index: 999;
+    background: #fafafa;
+    border: none;
+    outline: none;
+    padding-left: 55px;
+    padding-right: 55px;
+    color: #666;
+    font-weight: 400;
+
+}
+.fixed-item {
+  background: yellow;
+  border: 2px solid red;
+  height: 500px;
+  left: 30px;
+  top: 35px;
+  position: fixed;
+  width: 100px;
+  z-index: 999;
+}
 
 
 </style>
